f9c28693cb326bbb9128ffc0d20f5199c4f63526
Code with Cougar <codewithcougar@gmail.com>
2023-05-30 13:41:29 -0700

    first commit

---
+++ LinkedList/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            Student student2 = student1;
+        }
+    }
+}
